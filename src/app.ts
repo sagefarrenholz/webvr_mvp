0156a7816ebfa2055c6eae761cd4e1e7d863be01
BuildTools <unconfigured@null.spigotmc.org>
2021-01-01 22:32:19 -0500

    Added css-loader and updated app members

--- src/app.ts
+++ src/app.ts
@@ -5,32 +5,37 @@ import { Engine, Scene, ArcRotateCamera, Vector3,
     HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
 
 class App {
+    private a_canvas: HTMLCanvasElement;
+    private a_engine: Engine;
+    private a_scene: Scene;
+    private a_id;
+
     constructor () {
-        var canvas = document.createElement("canvas");
-        canvas.style.width = "100%";
-        canvas.style.height = "100%";
-        canvas.id = "gameCanvas";
-        document.body.appendChild(canvas);
+        this.a_canvas = document.createElement("canvas");
+        this.a_canvas.style.width = "100%";
+        this.a_canvas.style.height = "100%";
+        this.a_canvas.id = "gameCanvas";
+        document.body.appendChild(this.a_canvas);
 
-        var engine: Engine = new Engine(canvas, true);
-        var scene: Scene = new Scene(engine);
-        var camera: ArcRotateCamera = new ArcRotateCamera("arcCamera", Math.PI / 2, Math.PI / 2, 3, new Vector3(0, 0, 0), scene);
-        camera.attachControl(canvas);
-        var light: HemisphericLight = new HemisphericLight("hemiLight", new Vector3(0, 1, 0), scene);
-        const sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+        this.a_engine = new Engine(this.a_canvas, true);
+        this.a_scene = new Scene(this.a_engine);
+        var camera: ArcRotateCamera = new ArcRotateCamera("arcCamera", Math.PI / 2, Math.PI / 2, 3, new Vector3(0, 0, 0), this.a_scene);
+        camera.attachControl(this.a_canvas);
+        var light: HemisphericLight = new HemisphericLight("hemiLight", new Vector3(0, 1, 0), this.a_scene);
+        const sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, this.a_scene);
 
-        window.addEventListener("keydown", function(ev){
+        window.addEventListener("keydown", (ev) => {
             if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
-                if (scene.debugLayer.isVisible()) {
-                    scene.debugLayer.hide();
+                if (this.a_scene.debugLayer.isVisible()) {
+                    this.a_scene.debugLayer.hide();
                 } else {
-                    scene.debugLayer.show();
+                    this.a_scene.debugLayer.show();
                 }
             }
         });
 
-        engine.runRenderLoop(() => {
-            scene.render();
+        this.a_engine.runRenderLoop(() => {
+            this.a_scene.render();
         })
     }
 
